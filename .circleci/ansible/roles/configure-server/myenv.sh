e405def3d0c6dea661c9e7aa97aa0f3de7f96e80
Kingsley Ocran <ocrankingsley@gmail.com>
2023-02-06 01:33:27 +0000

    configure run-migration job

---
+++ .circleci/ansible/roles/configure-server/myenv.sh
@@ -0,0 +1,10 @@
+
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_MIGRATIONS=./migrations/*.js
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity.js
+export TYPEORM_HOST=udapeople-database.cpsjxf5t21ur.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres
